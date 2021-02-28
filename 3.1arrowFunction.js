2159c0c87bc70e4a9b9e5c70ea26d7c7e5b5a17e
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-02-28 15:12:03 +0700

    Update 3.1arrowFunction.js

--- 3.1arrowFunction.js
+++ 3.1arrowFunction.js
@@ -1,1 +1,54 @@
+// ARROW FUNCTION
 
+// bentuk lain yang lebih ringkas dari function Expression
+
+
+// function Expression
+const tampilNama = function (nama) {
+	return `Halo, ${nama}`;
+}
+console.log(tampilNama('Riki Widiantoro'));
+
+
+// merubah menjadi arrow function
+const tampilNamaa = (namaa) => {
+	return `Haloo, ${namaa}`;
+}
+console.log(tampilNamaa('Toro'));
+
+
+// jika parameter 2
+const tampilNamaaa = (namaaa, waktu) => {
+	return `Selamat ${waktu}, ${namaaa}`;
+}
+console.log(tampilNamaaa('Eren', 'Pagi'));
+
+
+// jika parameternya cuma satu | tidak perlu kurung buka tutup, return, kurung kurawa teknik ini disebut implisit return
+const tampilName = name => `Halo, ${name}`;
+console.log(tampilName('Riki Widiantoro'));
+
+
+// juka tanpa parameter wajib memakai kurung buka tutup nya
+const halo = () => `Hello World`;
+console.log(halo());
+
+
+
+// contoh lain arrow function dengan menggunakan function map pada javascript untuk memetakan sebuah function ke dalam array
+let mahasiswa = ['riki', 'widia', 'torong'];
+
+// dengan function biasa
+// let jmlHuruf = mahasiswa.map(function(nama) {
+// 	return nama.length;
+// });
+// console.log(jmlHuruf);
+
+// dengan arrow function
+// let jmlHuruf = mahasiswa.map( nama => nama.length);
+// console.log(jmlHuruf);
+
+// mengembalikan object, buka array lagi
+let jmlHuruf = mahasiswa.map( nama => ({
+	nama: nama, jmlHuruf: nama.length}));
+console.table(jmlHuruf);
